6f44ff537b52675905d9353c3424f024fa6b178f
3arglop <estefania.arguetalopez13@gmail.com>
2022-10-17 10:21:12 -0500

    Added an alert and prompt to my external.js

--- JS/external.js
+++ JS/external.js
@@ -1,3 +1,9 @@
 "use strict";
 
 console.log("Hello from EXTERNAL JS, woooo!");
+
+alert("Welcome to my Website!");
+
+let userColor = prompt("What is your favorite color?");
+
+alert(`Cool, ${userColor} is my favorite color too!`);
