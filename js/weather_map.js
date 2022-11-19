94e0b9896bf6a31da342622ad194adc2bcf3d820
3arglop <estefania.arguetalopez13@gmail.com>
2022-11-19 16:38:38 -0600

    Added MAPBOX functionality, my OPENWEATHER call is responding. I am creating new HTML for cards but need to extract the info first

--- js/weather_map.js
+++ js/weather_map.js
@@ -21,6 +21,46 @@ map.addControl(new mapboxgl.NavigationControl());
 
 
 //TODO: CALLING THE DOM
+const displayCityName = $('#cityName');
+console.log(displayCityName);
+const displayForecast = $('#showForecast');
+console.log(displayForecast);
 
+//TODO: CREATING THE DYNAMIC HTML FOR MY CARD (SINGLE)
+const renderForecastCard = (oneDayForecast) => {
+    let html = `<div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="card-link">Card link</a>
+                    <a href="#" class="card-link">Another link</a>
+                </div>
+            </div>`
 
+    return html;
+}
+
+//TODO: FETCHING OPEN WEATHER DATA BY CALLING THE API
+const fetchWeatherData = (cityName) => {
+    const openWeatherKey = OPEN_WEATHER;
+    const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&appid=${openWeatherKey}`
+
+    fetch(URL)
+        .then(response => {
+            console.log(response);
+            if (!response.ok) {
+                throw Error('ERROR');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data.list);
+            console.table(data.list[0]);
+            console.log(data.list[0].dt_txt);
+        });
+}
+fetchWeatherData("dallas");
+// fetchWeatherData("london");
+// fetchWeatherData("mexico city")
 
