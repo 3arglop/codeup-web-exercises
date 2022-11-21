0a5ab306d087db4e69a0dbe1bb87a3982e60c1c5
3arglop <estefania.arguetalopez13@gmail.com>
2022-11-21 13:00:36 -0600

    5DAY WEATHER APP: I was able to get a lot done but it seems like my database is not the best... I will need to construct a better one :/

--- js/weather_map.js
+++ js/weather_map.js
@@ -2,62 +2,16 @@
 
 console.log("Hello from Weather Map JS 😜");
 
-
-//TODO: CALLING THE MAPBOX API AND RENDERING MAP INTO MY HTML
-mapboxgl.accessToken = MAPBOX_TOKEN;
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/dark-v10', // style URL
-    center: [-95.7129, 37.0902], // starting position [lng, lat]
-    zoom: 3.5, // starting zoom'
-    projection: 'mercator'
-});
-map.on('style.load', () => {
-    map.setFog({});
-});
-
-map.addControl(new mapboxgl.NavigationControl());
-
-
-
 //TODO: CALLING THE DOM
 const displayCityName = $('#cityName');
-console.log(displayCityName);
-const displayForecast = $('#showForecast');
-console.log(displayForecast);
+const displayCoords = $('#showCoords');
+const displayForecast = document.getElementById('showForecast');
 
-//TODO: CREATING THE DYNAMIC HTML FOR MY CARD (SINGLE)
-const renderForecastCard = (oneDayForecast) => {
-    let html = `<div class="card" style="width: 18rem;">
-                <div class="card-body text-center">
-                    <h6 class="card-subtitle mb-2 text-muted">2202-11-20</h6>
-                    <h5 class="card-title">42.01</h5>
-                    <div>
-                        <span>41.94</span>
-                        <span>46.11</span>
-                    </div>
-                    <img src="IMG/codeup-logo.png" alt="" width="95px" height="80px">
-                    <div class="card-header">
-                        <span>scattered clouds</span>
-                    </div>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Wind Speed: 5.57 mph</li>
-                        <li class="list-group-item">Pressure: 1031 inHg</li>
-                        <li class="list-group-item">Humidity: 51%</li>
-                    </ul>
-                </div>
-            </div>`
-
-    return html;
-}
-
-//TODO: CREATING AN EMPTY ARRAY WHERE I WILL EXTRACT AND PUSH THE DATA TO
-const fiveDayForecast = [];
 
 //TODO: FETCHING OPEN WEATHER DATA BY CALLING THE API
 const fetchWeatherData = (cityName) => {
     const openWeatherKey = OPEN_WEATHER;
-    const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${openWeatherKey}`
+    const URL = `https://api.openweathermap.org/data/2.5/forecast/?q=${cityName}&units=imperial&cnt=40&appid=${openWeatherKey}`
 
     fetch(URL)
         .then(response => {
@@ -68,54 +22,198 @@ const fetchWeatherData = (cityName) => {
             return response.json();
         })
         .then(data => {
-            console.log(data.list);
+            console.log(data);
+            // console.table(data);
 
-            console.table(data.list[0]);
-            console.table(data.list[0].main);
-            console.table(data.list[0].weather);
-            console.table(data.list[0].wind);
+            //TODO: CONVERTING OPEN WEATHER'S DATA INTO MY OWN DATABASE
+            const weatherData = data;
+            const newData = myDataBase(weatherData);
 
-            // console.log(data.list[0].dt_txt);
-            // console.log(data.list[0].main.temp);
-            // console.log(data.list[0].main.temp_min);
-            // console.log(data.list[0].main.temp_max);
-            // console.log(data.list[0].weather[0].description);
-            // console.log(data.list[0].wind.speed);
-            // console.log(data.list[0].main.pressure);
-            // console.log(data.list[0].main.humidity);
+            console.log(newData);
+            // console.table(newData);
 
-            const timeStamps = data.list.map(time => {
-                for(let i = 0; i < data.list.length; i += 9) {
-                    console.log(time[i].dt_txt);
-                }
-            });
-            console.table(timeStamps);
-            const tempStamps = data.list.map(temp => temp.main.temp);
-            console.table(tempStamps);
-            const tempMinStamps = data.list.map(minTemp => minTemp.main.temp_min);
-            console.table(tempMinStamps);
-            const tempMaxStamps = data.list.map(maxTemp => maxTemp.main.temp_max);
-            console.table(tempMaxStamps);
+            //TODO: RUNNING MY FUNCTIONS TO RENDER DYNAMIC HTML BASED ON THE USERS INPUT
+            renderCityCountry(newData);
+            renderCoords(newData);
+            placeMarkerAndPopup(newData, MAPBOX_TOKEN, map);
+            // renderAllForecastCards(newData);
 
-            //TODO: EXTRACTING DATA
-            const weatherInfo = data.list.map(weather => weather.weather);
-            console.table(weatherInfo[0]);
-            for(let single of weatherInfo) {
-                for(let i = 0; i < single.length; i++) {
-                    console.log(single[i].description);
-                }
-            }
-
-            const speedStamps = data.list.map(speed => speed.wind.speed);
-            console.table(speedStamps);
-            const pressureStamps = data.list.map(pressure => pressure.main.pressure);
-            console.table(pressureStamps);
-            const humidityStamps = data.list.map(humidity => humidity.main.humidity);
-            console.table(humidityStamps);
+            // console.log(newData.time);
+            // console.log(Object.keys(newData));
+            // console.log(Object.values(newData));
+            // const dataEntries = Object.entries(newData);
 
         });
 }
-fetchWeatherData("dallas");
-// fetchWeatherData("london");
-// fetchWeatherData("mexico city")
 
+//TODO: FUNCTION TO CONVERT AN ARRAY INTO A STRING
+
+// function iterateThruData(data) {
+//     let value;
+//     let eachDataInfo;
+//     const newArr = [];
+//     for(let items of Object.entries(data)) {
+//         // for(let i = 0; value.length; i++) {
+//         //     eachDataInfo = value[i];
+//         //     console.log(eachDataInfo);
+//         // }
+//     }
+//     // console.log(eachDataInfo);
+//
+//     console.log(value);
+//
+//     // newArr.push(value);
+//     //
+//     // console.log(newArr);
+//     // console.log(newArr.length);
+//     //
+//     // return newArr;
+// }
+
+
+//TODO: FUNCTION TO EXTRACT DATA
+
+function myDataBase(weatherData) {
+
+    //TODO: CREATING MY NEW DATABASE
+    const fiveDayForecast = {};
+
+    //TODO: USING THE MAP METHOD TO EXTRACT THE DATA
+    fiveDayForecast.cityName = weatherData.city.name;
+    fiveDayForecast.countryName = weatherData.city.country;
+    fiveDayForecast.coordinates = weatherData.city.coord;
+
+    fiveDayForecast.time = weatherData.list.map(time => time.dt_txt);
+    fiveDayForecast.temperature = weatherData.list.map(temp => temp.main.temp);
+    fiveDayForecast.min = weatherData.list.map(minTemp => minTemp.main.temp_min);
+    fiveDayForecast.max = weatherData.list.map(maxTemp => maxTemp.main.temp_max);
+
+    const weatherInfo = weatherData.list.map(weather => weather.weather);
+    fiveDayForecast.description = weatherInfo.map(single => {
+        for(let i = 0; i < single.length; i++) {
+            const eachDescription = single[i].description;
+            return eachDescription;
+        }
+    });
+    fiveDayForecast.icon = weatherInfo.map(single => {
+        for(let i = 0; i < single.length; i++) {
+            const eachIcon = single[i].icon;
+            return eachIcon;
+        }
+    });
+
+    fiveDayForecast.speed = weatherData.list.map(speed => speed.wind.speed);
+    fiveDayForecast.pressure = weatherData.list.map(pressure => pressure.main.pressure);
+    fiveDayForecast.humidity = weatherData.list.map(humidity => humidity.main.humidity);
+
+    // console.log(fiveDayForecast);
+    // console.log(typeof fiveDayForecast);
+
+    return fiveDayForecast;
+}
+
+
+//TODO: CALLING THE MAPBOX API AND RENDERING MAP INTO MY HTML
+mapboxgl.accessToken = MAPBOX_TOKEN;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/dark-v10', // style URL
+    center: [-95.7129, 37.0902], // starting position [lng, lat]
+    zoom: 2, // starting zoom'
+    projection: 'mercator'
+});
+map.on('style.load', () => {
+    map.setFog({});
+});
+map.addControl(new mapboxgl.NavigationControl());
+
+
+//TODO: CREATING MARKER AND POPUP TO SIGNAL WHERE THE USER IS AT IN THE MAP
+const placeMarkerAndPopup = (data, token, map) => {
+    geocode(data.cityName, MAPBOX_TOKEN).then((coordinates) => {
+        console.log(coordinates);
+        let popup = new mapboxgl.Popup()
+            .setHTML(`<h5 class="px-3 my-2">${data.cityName},<br> ${data.countryName}</h5>`);
+        let marker = new mapboxgl.Marker({
+            color: "gold",
+            draggable: false
+        })
+            .setLngLat(coordinates)
+            .setPopup(popup)
+            .addTo(map);
+    });
+}
+
+//TODO: THIS FUNCTION WILL RENDER THE CITY INFORMATION
+const renderCityCountry = (data) => {
+    let html = "";
+    html = `<h4>${data.cityName}, ${data.countryName}</h4>`;
+
+    return displayCityName.html(html);
+}
+
+//TODO: THIS FUNCTION WILL RENDER THE COORDINATES
+const renderCoords = (data) => {
+    let html = "";
+    html = `<h5>Longitude:
+            <br>
+            <span id="currentLng">${data.coordinates.lon}</span>
+            </h5>
+            <h5>Latitude:
+            <br>
+            <span id="currentLat">${data.coordinates.lat}</span>
+            </h5>
+            </div>`;
+
+    return displayCoords.html(html);
+}
+
+//TODO: CREATING THE DYNAMIC HTML FOR MY CARD (SINGLE)
+
+// function renderAllForecastCards(oneDayForecast) {
+//     let html = "";
+//
+//     for(let i = 0; i < oneDayForecast.length; i+8) {
+//         console.log(oneDayForecast.time[i]);
+//         html += `<div class="card" style="width: 18rem;">
+//                 <div class="card-body text-center">
+//                     <h6 class="card-subtitle mb-2 text-muted">${oneDayForecast[i].time[i]}</h6>
+//                     <h5 class="card-title">${oneDayForecast.temperature[i]}</h5>
+//                     <div>
+//                         <span id="min">${oneDayForecast[i].max[i]}</span>
+//                         <span id="max">${oneDayForecast[i].min[i]}</span>
+//                     </div>
+//                     <img src="IMG/codeup-logo.png" alt="" width="95px" height="80px">
+//                     <div class="card-header">
+//                         <span>${oneDayForecast[i].description[i]}</span>
+//                     </div>
+//                     <ul class="list-group list-group-flush">
+//                         <li class="list-group-item">Wind Speed: ${oneDayForecast[i].speed[i]} mph</li>
+//                         <li class="list-group-item">Pressure: ${oneDayForecast[i].pressure[i]} inHg</li>
+//                         <li class="list-group-item">Humidity: ${oneDayForecast[i].humidity[i]}%</li>
+//                     </ul>
+//                 </div>
+//             </div>`
+//     }
+//
+//     console.log(html);
+//     return displayForecast.innerHTML = html;
+// }
+
+
+//TODO: THIS FUNCTION WILL RENDER ALL 5 CARDS FOR EACH DAY USING MY NEW DATABASE
+
+// const renderAllForecastCards = (fiveForecast) => {
+//     let html = '';
+//     for(let i = 0; i <  fiveForecast.length; i++) {
+//         if(fiveForecast[i] === 0 && fiveForecast[i] === 5) {
+//             return html += renderForecastCard(fiveForecast[i]);
+//         }
+//     }
+//     displayForecast.html(html);
+// }
+
+
+// fetchWeatherData("london");
+fetchWeatherData("mexico city");
+// fetchWeatherData("dallas");
